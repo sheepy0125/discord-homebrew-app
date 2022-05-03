396fd867de567b389d8f0cc00eae75d6c4ccfba0
sheepy0125 <sheepy404@gmail.com>
2022-05-02 20:43:42 -0400

    Handshake with proxy

--- make-script.sh
+++ make-script.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 # I don't know how to use Makefiles and am too lazy to learn. So, here is my make script.
-mkdir mounted_sd
-sudo mount /dev/sdc1 mounted_sd
-make && sudo cp target/3ds/release/rust3ds-discord.3dsx mounted_sd/3ds
-sudo umount mounted_sd
-rmdir mounted_sd
+
+DEVICE_NAME="sdc1"
+
+cleanup() {
+    echo "Cleaning up..."
+    echo "Waiting for SD card to finish..."
+    echo "Sleeping for 1 seconds to let the SD card finish writing..."
+    sleep 1
+    sudo umount mounted_sd
+    rmdir mounted_sd > /dev/null 2>&1
+}
+error() {
+    cleanup
+    figlet "FAILED!!!!"
+    exit 1
+}
+
+cleanup
+mkdir -p mounted_sd || error
+sudo mount /dev/$DEVICE_NAME mounted_sd || error
+make && sudo cp target/3ds/release/rust3ds-discord.3dsx mounted_sd/3ds || error
+cleanup
+figlet "SUCCESS!"
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,89 @@
+/// Discord for the 3DS
 extern crate ctru;
 extern crate ctru_sys as libctru;
 
-use ctru::gfx::Gfx;
+use ctru::applets::swkbd::{Button, Swkbd};
 use ctru::console::Console;
+use ctru::gfx::{Gfx, Screen};
 use ctru::services::apt::Apt;
 use ctru::services::hid::{Hid, KeyPad};
+use ctru::services::soc::Soc;
 
+use std::io::{Read, Write};
+use std::net::TcpStream;
+
+/// Constants
+const PREFIX: &str = "!";
+const BUFFER_SIZE: usize = 8 as usize;
+
+/* Functions */
+/// Shows a horizontal line as a border
+fn border_line() -> String {
+    let mut line = String::new();
+    for _ in 0..80 {
+        line.push('-');
+    }
+    line
+}
+
+/// Welcome message and instructions
+fn welcome_message() {
+    println!("Discord for the Nintendo 3DS!!!");
+    println!("Press 'start' to exit.");
+    println!("Press 'A' to type a message.");
+    println!("Press 'select' to send the message.");
+    println!("Press 'Y' to see this message again.");
+}
+
+/// Run
 fn main() {
     let apt = Apt::init().unwrap();
     let hid = Hid::init().unwrap();
     let gfx = Gfx::default();
     let _console = Console::default();
 
-    println!("Hello, world!");
+    // The top screen will be used to show the messages
+    let top_screen = Console::init(Screen::Top);
+    // The bottom screen will be used to show the keyboard and status messages
+    let bottom_screen = Console::init(Screen::Bottom);
 
-    println!("\x1b[29;16HPress Start to exit");
+    bottom_screen.select();
+    welcome_message();
+    border_line();
+
+    // Connect to Discord
+    println!("Connecting to Discord Proxy...");
+    println!("Initializing network...");
+    match Soc::init() {
+        Ok(soc) => {
+            println!("Successfully initialized Soc service");
+            let proxy_ip = "192.168.86.67:7000";
+            match TcpStream::connect(proxy_ip) {
+                Ok(mut stream) => {
+                    println!("Success! Sending hello...");
+                    let hello_message = b"HELLO3DS";
+                    stream.write(hello_message).unwrap();
+                    println!("Waiting for response...");
+                    let mut data = [0; 8]; // Length is 8
+                    match stream.read_exact(&mut data) {
+                        Ok(response) => {
+                            if &data == hello_message {
+                                println!("Hello succeeded!");
+                            } else {
+                                println!(
+                                    "Hello failed! Got {} instead!",
+                                    String::from_utf8_lossy(&data)
+                                );
+                            }
+                        }
+                        Err(e) => println!("Failed handshake: {}", e),
+                    }
+                }
+                Err(e) => println!("Failed to connect: {}", e),
+            }
+        }
+        Err(e) => println!("Failed to initialize Soc service: {}", e),
+    }
 
     while apt.main_loop() {
         gfx.flush_buffers();
@@ -22,11 +91,29 @@ fn main() {
         gfx.wait_for_vblank();
 
         hid.scan_input();
-        if hid.keys_down().contains(KeyPad::KEY_START) {
-            break;
+        let inputs = hid.keys_down();
+        // Show a keyboard for the message input
+        if inputs.contains(KeyPad::KEY_A) {
+            let mut keyboard = Swkbd::default();
+            let mut text = String::new();
+            // TODO: fix scope
+            match keyboard.get_utf8(&mut text) {
+                Ok(Button::Right) => println!("Message: {}", text),
+                Ok(Button::Left) => println!("Cancelled"),
+                Ok(Button::Middle) => {
+                    println!("what do you want me to do? save and not save the message?")
+                }
+                Err(_) => println!("Error!"),
+            }
         }
-        if hid.keys_down().contains(KeyPad::KEY_SELECT) {
-            panic!("what if i were to panic... on purpose >:)");
+        // Send the message
+        if inputs.contains(KeyPad::KEY_SELECT) {
+            // TODO: send the message
+            println!("Message sent!");
+        }
+        // Exit
+        if inputs.contains(KeyPad::KEY_START) {
+            break;
         }
     }
 }
